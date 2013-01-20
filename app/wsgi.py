67a611b1c7dacf193618477898695b04ed414cc9
Rodrigo Chacon <rochacon@gmail.com>
2013-01-20 17:04:21 -0200

    Testing charm upgrade

--- app/wsgi.py
+++ app/wsgi.py
@@ -7,3 +7,7 @@ def testing():
     return 'Hello <a href="https://juju.ubuntu.com">JuJu</a>!!'
 
 
+@application.route('/works')
+def it_works():
+    return 'JuJu powered server w/ charm upgrading.', 202
+
